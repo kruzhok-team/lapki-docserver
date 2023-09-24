c57430ca26ffdd02eb329b395c3eb65215f8820d
Mikhail Chekan <chekoopa@mail.ru>
2023-09-25 00:34:39 +0800

    первая публичная ревизия

---
+++ make_index.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import os
+import json
+
+keyName = lambda d: d['name']
+
+def traverse(basedir):
+    files = []
+    for name in os.listdir(basedir):
+        path = os.path.join(basedir, name)
+        if os.path.isdir(path):
+            files.append({'name': name, 'children': traverse(path)})
+        else:
+            name = name.rsplit(".", 1)[0]
+            files.append({'name': name, 'path': path})
+    return sorted(files, key=keyName)
+
+def main():
+    BASEDIR = 'docs'
+    index_data = {
+        'version': 1, 
+        'body': {
+            'name': 'index',
+            # 'path': '',
+            'children': traverse(BASEDIR),
+        },
+    }
+    with open('index.json', 'w', encoding='utf-8') as fout:
+        json.dump(index_data, fout, ensure_ascii=False, indent=4)
+
+if __name__ == "__main__":
+    main()
